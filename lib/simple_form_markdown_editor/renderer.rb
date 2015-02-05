74949ace41e696a46cc22e19f10e8422c1edd040
Tomas Celizna <tomas.celizna@gmail.com>
2015-02-05 17:33:40 +0100

    Create renderer.rb

---
+++ lib/simple_form_markdown_editor/renderer.rb
@@ -0,0 +1,27 @@
+module SimpleFormMarkdownEditor
+  class Renderer
+  
+      def self.call(*args)
+        new(*args).call
+      end
+      
+      # =====================================================================
+      
+      attr_reader :str
+      
+      def initialize str
+        @str = str
+      end
+      
+      def call
+        to_html
+      end
+      
+      private # =============================================================
+      
+      def to_html
+        # TODO: turn @str to html
+      end
+      
+  end
+end
