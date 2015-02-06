71ecc41ae8aedb99dc69b715df673c108707a113
Asger Behncke Jacobsen <asger@8kilo.com>
2015-02-06 13:43:54 +0100

    fix config

--- lib/simple_form_markdown_editor/configuration.rb
+++ lib/simple_form_markdown_editor/configuration.rb
@@ -2,12 +2,23 @@ module SimpleFormMarkdownEditor
   class Configuration
 
     attr_accessor :commands
+    attr_accessor :extensions
+    attr_accessor :render_options
 
     def initialize
       @commands = {
         edit: 'Write',
         preview: 'Preview'
       }
+      @extensions = {
+        autolink: true,
+        footnotes: true,
+        highlight: true,
+        space_after_headers: true,
+        strikethrough: true,
+        superscript: true
+      }
+      @render_options = {}
     end
 
   end
