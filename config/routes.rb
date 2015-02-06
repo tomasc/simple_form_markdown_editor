b9ef195b31bfecb75f3d1c81578bf0e88ba196e3
Asger Behncke Jacobsen <asger@8kilo.com>
2015-02-06 13:45:10 +0100

    doubt the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# NOTE: Is this the best way to do it? Users will have to mount the engine. Alternative?
+
 SimpleFormMarkdownEditor::Engine.routes.draw do
   scope module: :simple_form_markdown_editor do
     post "/preview" => "previews#preview", as: :preview
